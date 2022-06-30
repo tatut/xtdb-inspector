ef895bfdd2d7e023de07623c6bba6c7cc9864cb4
Tatu Tarvainen <tatu.tarvainen@me.com>
2022-06-30 21:33:54 +0300

    Add WIP graphs tab to attr page

--- src/xtdb_inspector/page/attr.clj
+++ src/xtdb_inspector/page/attr.clj
@@ -6,7 +6,10 @@
             [xtdb-inspector.ui.table :as ui.table]
             [xtdb-inspector.util :refer [enc]]
             [ripley.live.source :as source]
-            [xtdb-inspector.id :as id]))
+            [xtdb-inspector.id :as id]
+            [xtdb-inspector.ui.tabs :as ui.tabs]
+            [xtdb-inspector.ui.chart :as ui.chart]
+            [ripley.integration.xtdb :as rx]))
 
 (defn- request-attr [request]
   (let [{kw' :keyword
@@ -30,29 +33,50 @@
                                  :limit (inc limit)}))))
                 limit-source)]
     (h/html
-     [:div
-      (ui.table/table
-       {:columns [{:label "Document" :accessor :doc
-                   :render (fn [doc]
-                             (ui/format-value (constantly true) doc))}
-                  {:label "Value" :accessor :val
-                   :render (fn [{:keys [id? v]}]
-                             (ui/format-value (constantly id?) v))}]
-        :key first}
-       values)
-      (h/html
-       [::h/live (source/computed
-                  (fn [values limit]
-                    {:count (count values)
-                     :limit limit}) values limit-source)
-        (fn [{:keys [count limit]}]
+     (ui.tabs/tabs
+      {:label "Values"
+       :render
+       (fn []
+         [:div
+          (ui.table/table
+           {:columns [{:label "Document" :accessor :doc
+                       :render (fn [doc]
+                                 (ui/format-value (constantly true) doc))}
+                      {:label "Value" :accessor :val
+                       :render (fn [{:keys [id? v]}]
+                                 (ui/format-value (constantly id?) v))}]
+            :key first}
+           values)
           (h/html
-           [:div
-            [::h/when (> count limit)
-             [:div
-              [:div.text-xs "Query limited to " limit " items"]
-              [:button {:class "px-4 py-2 bg-indigo-50 outline-none border border-indigo-100 rounded text-indigo-500 font-medium active:scale-95 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
-                        :on-click #(set-limit! (* 2 limit))} "Fetch more results"]]]]))])])))
+           [::h/live (source/computed
+                      (fn [values limit]
+                        {:count (count values)
+                         :limit limit}) values limit-source)
+            (fn [{:keys [count limit]}]
+              (h/html
+               [:div
+                [::h/when (> count limit)
+                 [:div
+                  [:div.text-xs "Query limited to " limit " items"]
+                  [:button {:class "px-4 py-2 bg-indigo-50 outline-none border border-indigo-100 rounded text-indigo-500 font-medium active:scale-95 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
+                            :on-click #(set-limit! (* 2 limit))} "Fetch more results"]]]]))])])}
+      {:label "Graphs"
+       :render
+       (fn []
+         (let [attr-name (pr-str attr)]
+           (h/html
+            [:div.mx-2
+             "Values for "
+             [:span.font-mono attr-name]
+             " by count"
+             ;; PENDING: use viewBox to standardize the space in rendering
+             ;; and use some proportional size here
+             (ui.chart/bar-chart {:width 500 :bar-height 30}
+                                 (rx/q {:node xtdb-node}
+                                       {:find '[v (count v)]
+                                        :keys '[label value]
+                                        :where [['_ attr 'v]]
+                                        :group-by ['v]}))])))}))))
 
 (defn- render-attr-listing [{:keys [xtdb-node]}]
   (ui.table/table
