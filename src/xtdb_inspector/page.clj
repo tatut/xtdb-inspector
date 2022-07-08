ff15b16d93976b4dadbe8175b286818f30462ffa
Tatu Tarvainen <tatu.tarvainen@me.com>
2022-07-08 19:36:20 +0300

    Add cypress test for lucene search

--- src/xtdb_inspector/page.clj
+++ src/xtdb_inspector/page.clj
@@ -50,8 +50,8 @@
            {:d "M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"}]]]]
 
        [:template {:x-if (str "search || " results ".length > 0")
-                   :x-effect (str results "; search = false")}
-        [:table {:style "left:12.5%;top:50px;" :class "z-50 fixed w-9/12 bg-gray-100 p-2 border-2"}
+                   :x-effect (str term "; search = false")}
+        [:table.lucene-results {:style "left:12.5%;top:50px;" :class "z-50 fixed w-9/12 bg-gray-100 p-2 border-2"}
          [:template {:x-if "search"}
           [:tr
            [:td {:colspan 3}
