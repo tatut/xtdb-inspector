1cf384141ce208d1d7e2c1693096c7246b1246f6
Tatu Tarvainen <tatu.tarvainen@me.com>
2022-07-10 15:15:19 +0300

    Try with $ syntax

--- cypress/cypress.sh
+++ cypress/cypress.sh
@@ -10,7 +10,7 @@ MAX_WAIT=30
 until $(curl --output /dev/null --silent --fail http://localhost:3000/doc); do
     echo 'Waiting for XTDB inspector to be up'
     sleep 1
-    ((MAX_WAIT--))
+    (($MAX_WAIT--))
     if [ $MAX_WAIT -eq 0 ]; then
         echo "XTDB inspector didn't start in 30s, exiting"
         exit 1
